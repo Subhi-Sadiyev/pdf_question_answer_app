96e7f8504f1bffedcb76c5ff392200bfcc820151
Subhi-Sadiyev <93770352+Subhi-Sadiyev@users.noreply.github.com>
2024-09-23 18:22:48 +0400

    changed some redundant code

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -11,9 +11,3 @@ def extract_text_from_entire_pdf(pdf_path):
         page = pdf_document.load_page(page_num)
         text += page.get_text("text")
     return text
-
-## example of applying the function to local pdf
-#pdf_path = 'etibarsiz_techizatchilar_haqqinda_eng.pdf'
-#pdf_text = extract_text_from_entire_pdf(pdf_path)
-
-#print(pdf_text)
